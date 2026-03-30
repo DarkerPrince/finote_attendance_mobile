98c01273c51dff4c8bae959613becfd50a4aa71c
leule <luelsisay42@gmail.com>
2026-03-30 05:41:57 +0300

    api is back to prod instead of being local

--- lib/Constants/StringConstants.dart
+++ lib/Constants/StringConstants.dart
@@ -1,2 +1,2 @@
-// const String baseUrl = "https://api.luel.dev";
-const String baseUrl = "http://192.168.1.4:5001";
+const String baseUrl = "https://api.luel.dev";
+// const String baseUrl = "http://192.168.1.4:5001";
